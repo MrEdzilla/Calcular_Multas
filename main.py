f09ca2e5dadd23f8dcbbac4dbc7dbe31f48397b9
MrEdzilla <43966574+MrEdzilla@users.noreply.github.com>
2025-07-23 13:03:22 +0100

    Eliminada a variável "tipo de via"

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 
 velocidade = float(input("Registe a velocidade do automóvel ligeiro: "))
-tipo_via = input("Registe o tipo de via em que circulava (localidade / fora / autoestrada)").strip().lower()
 
 def multa_localidade(velocidade):
     if velocidade <= 50:
